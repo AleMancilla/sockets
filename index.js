8896795b1825f04ba324408035bcdc6b971be37e
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-02-18 00:09:19 -0400

    creamos la carpeta public para todo lo que sera web, adicionamos index.html y conectamos con path desde index.js path variable de entorno de nodeJS

--- index.js
+++ index.js
@@ -2,12 +2,20 @@
 //archivo principal de mi proyecto
 //console.log('Hello world 2!');
 
+const path =require('path'); // modulo de NodeJS para unir directorios
 const express = require('express');
 const app = express();
 
 //setings
 //configura el puerto , toma por defecto del sistema operativo o si no hay tomara el puerto 3000
 app.set('port', process.env.PORT || 3000); 
+
+//static files
+//enviamos los archivos al navegador
+//console.log(__dirname + '\public')
+//console.log(path.join(__dirname, 'public'));//esto se encarga de window linux sin que nos preocupemos
+app.use(express.static(path.join(__dirname, 'public')))
+//app.use(express.static())
 
 //start server
 app.listen(app.get('port'),() =>{

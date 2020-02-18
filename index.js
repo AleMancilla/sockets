eae150ec3be8d171fcc01ade70813739aca93bcf
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-02-18 00:35:24 -0400

    configurando la coneccion socket de la parte de las configuraciones script y html

--- index.js
+++ index.js
@@ -6,18 +6,31 @@ const path =require('path'); // modulo de NodeJS para unir directorios
 const express = require('express');
 const app = express();
 
-//setings
+////#####setings
 //configura el puerto , toma por defecto del sistema operativo o si no hay tomara el puerto 3000
 app.set('port', process.env.PORT || 3000); 
 
-//static files
+////#####static files
 //enviamos los archivos al navegador
 //console.log(__dirname + '\public')
 //console.log(path.join(__dirname, 'public'));//esto se encarga de window linux sin que nos preocupemos
 app.use(express.static(path.join(__dirname, 'public')))
 //app.use(express.static())
 
-//start server
-app.listen(app.get('port'),() =>{
+////#####start server
+const server = app.listen(app.get('port'),() =>{
     console.log('server on port',app.get('port'))
 });
+
+//###################################333
+
+const SocketIO = require('socket.io');//configra comunicacion
+//le damos un servidor para usar la comunicacion bidireccional
+//SocketIO.listen(server);//ya tenemos configurado
+const io = SocketIO(server);//mantiene la coneccion
+
+//#####websockets
+io.on('connection',(socket)=>{
+    console.log('new connection', socket.id);
+    
+});//empezamos a escuchar cuando se conecta
--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,3 @@
 console.log('chat codigo');
+//io() //solo se conecta en el mismo dominio.. si usamos otro se le debe poner el parametro io('http//asdasdad')
+const socket = io()

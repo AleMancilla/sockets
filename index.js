1c6876c1e9f2e479ced4df861dd20dfc7946d4e4
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-02-18 00:00:50 -0400

    configuramos el puerto y lo conectamos al servidor

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 //archivo que arrancara mi servidor
 //archivo principal de mi proyecto
-console.log('Hello world 2!');
+//console.log('Hello world 2!');
+
+const express = require('express');
+const app = express();
+
+//setings
+//configura el puerto , toma por defecto del sistema operativo o si no hay tomara el puerto 3000
+app.set('port', process.env.PORT || 3000); 
+
+//start server
+app.listen(app.get('port'),() =>{
+    console.log('server on port',app.get('port'))
+});

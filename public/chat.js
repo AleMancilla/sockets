8f0cf17631edb039269c7f7a6a79561ac28fe75e
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-02-18 01:01:38 -0400

    atrapamos con exito username y message y lo imprimimos en el console mediante butonn

--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,15 @@
 console.log('chat codigo');
 //io() //solo se conecta en el mismo dominio.. si usamos otro se le debe poner el parametro io('http//asdasdad')
 const socket = io()
+
+//DOM ELEMENTS
+let message = document.getElementById('message');
+let username = document.getElementById('username');
+let btn = document.getElementById('send');
+let output = document.getElementById('output');
+let actions = document.getElementById('actions');
+
+btn.addEventListener('click', function(){
+    console.log(username.value, message.value);
+    
+});
